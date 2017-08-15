ed566380c7d61c10a53101866b49698c53e9fcb0
zjun <565061763@qq.com>
2017-08-15 15:29:42 +0800

    demo测试

--- app/src/main/java/com/fanwe/www/blocker/MainActivity.java
+++ app/src/main/java/com/fanwe/www/blocker/MainActivity.java
@@ -1,30 +1,23 @@
 package com.fanwe.www.blocker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 
-import com.fanwe.library.blocker.SDOnClickBlocker;
-
-public class MainActivity extends AppCompatActivity implements View.OnClickListener
+public class MainActivity extends AppCompatActivity
 {
-    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        SDOnClickBlocker.setGlobalBlockDuration(1000);
-        SDOnClickBlocker.setOnClickListener(findViewById(R.id.btn), this);
     }
 
-    @Override
-    public void onClick(View v)
+    public void onClickBlocker(View v)
     {
-        Log.i(TAG, "onClick:" + v);
+        startActivity(new Intent(this, OnClickBlockerActivity.class));
     }
 
 }
---
+++ app/src/main/java/com/fanwe/www/blocker/OnClickBlockerActivity.java
@@ -0,0 +1,61 @@
+package com.fanwe.www.blocker;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.fanwe.library.blocker.SDOnClickBlocker;
+
+public class OnClickBlockerActivity extends AppCompatActivity
+{
+
+    private TextView tv_click_count;
+    private int mClickCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onclick_blocker);
+        tv_click_count = (TextView) findViewById(R.id.tv_click_count);
+
+        //未拦截Button
+        findViewById(R.id.btn_normal).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                increaseClickCount();
+            }
+        });
+
+        //全局拦截Button，1000毫秒才可以触发一次
+        SDOnClickBlocker.setGlobalBlockDuration(1000); //设置全局拦截间隔
+        SDOnClickBlocker.setOnClickListener(findViewById(R.id.btn_global), new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                increaseClickCount();
+            }
+        });
+
+        //单独拦截Button，2000毫秒才可以触发一次
+        SDOnClickBlocker.setOnClickListener(findViewById(R.id.btn_private), 2000, new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                increaseClickCount();
+            }
+        });
+    }
+
+    private void increaseClickCount()
+    {
+        mClickCount++;
+        tv_click_count.setText(String.valueOf(mClickCount));
+    }
+
+}
