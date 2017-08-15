4d73788af7bfb6bfbe9215cf2632dd0e600d0886
zjun <565061763@qq.com>
2017-08-15 15:17:07 +0800

    demo测试

--- app/src/main/java/com/fanwe/www/blocker/MainActivity.java
+++ app/src/main/java/com/fanwe/www/blocker/MainActivity.java
@@ -1,15 +1,30 @@
 package com.fanwe.www.blocker;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity
+import com.fanwe.library.blocker.SDOnClickBlocker;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SDOnClickBlocker.setGlobalBlockDuration(1000);
+        SDOnClickBlocker.setOnClickListener(findViewById(R.id.btn), this);
     }
+
+    @Override
+    public void onClick(View v)
+    {
+        Log.i(TAG, "onClick:" + v);
+    }
+
 }
