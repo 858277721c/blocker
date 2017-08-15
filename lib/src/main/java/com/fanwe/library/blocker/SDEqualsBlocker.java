2c8195193be690a51aa9e75047a31b730e08ef70
zjun <565061763@qq.com>
2017-08-15 22:37:59 +0800

    新增SDEqualsBlocker

---
+++ lib/src/main/java/com/fanwe/library/blocker/SDEqualsBlocker.java
@@ -0,0 +1,69 @@
+package com.fanwe.library.blocker;
+
+/**
+ * 可以根据对象equals()的次数拦截事件
+ */
+public class SDEqualsBlocker
+{
+    /**
+     * 最后一次保存的equals相同的对象
+     */
+    private Object mLastEqualsObject = new Object();
+    /**
+     * 最大可以equals的次数
+     */
+    private int mMaxEqualsCount;
+    /**
+     * 当前equals的次数
+     */
+    private int mEqualsCount;
+
+    /**
+     * 设置最大可以equals的次数
+     *
+     * @param maxEqualsCount
+     */
+    public synchronized void setMaxEqualsCount(int maxEqualsCount)
+    {
+        this.mMaxEqualsCount = maxEqualsCount;
+    }
+
+    private void saveLastObject(Object lastObject)
+    {
+        this.mLastEqualsObject = lastObject;
+    }
+
+    /**
+     * 重置equals相同的次数
+     */
+    public synchronized void resetEqualsCount()
+    {
+        mEqualsCount = 0;
+    }
+
+    /**
+     * 触发对象拦截
+     *
+     * @param object
+     * @return true-拦截掉
+     */
+    public synchronized boolean blockObject(Object object)
+    {
+        if (mLastEqualsObject.equals(object))
+        {
+            mEqualsCount++;
+            if (mEqualsCount > mMaxEqualsCount)
+            {
+                mEqualsCount--;
+                return true;
+            }
+        } else
+        {
+            resetEqualsCount();
+        }
+
+        saveLastObject(object);
+        return false;
+    }
+
+}
