02eadc1f3148987b08477401a8c6a898f01cefd9
zjun <565061763@qq.com>
2017-08-16 09:33:03 +0800

    新增SDEqualsDurationBlocker

---
+++ lib/src/main/java/com/fanwe/library/blocker/SDEqualsDurationBlocker.java
@@ -0,0 +1,76 @@
+package com.fanwe.library.blocker;
+
+/**
+ * 可以根据时间间隔和对象equals()是否相等来拦截事件
+ */
+public class SDEqualsDurationBlocker extends SDDurationBlocker
+{
+    private SDEqualsBlocker mEqualsBlocker = new SDEqualsBlocker();
+    /**
+     * 拦截equals相同对象的时间间隔
+     */
+    private long mBlockEqualsDuration;
+
+    public SDEqualsDurationBlocker()
+    {
+        super();
+        setAutoSaveLastBlockTime(false);
+    }
+
+    @Override
+    public synchronized void setAutoSaveLastBlockTime(boolean autoSaveLastBlockTime)
+    {
+        super.setAutoSaveLastBlockTime(false);
+    }
+
+    /**
+     * 设置拦截equals相同对象的时间间隔
+     *
+     * @param blockEqualsDuration
+     */
+    public synchronized void setBlockEqualsDuration(long blockEqualsDuration)
+    {
+        this.mBlockEqualsDuration = blockEqualsDuration;
+    }
+
+    /**
+     * 当前是否处于拦截equals相同对象的间隔之内
+     *
+     * @return
+     */
+    public synchronized boolean isInBlockEqualsDuration()
+    {
+        long duration = System.currentTimeMillis() - getLastBlockTime();
+        return duration < mBlockEqualsDuration;
+    }
+
+    /**
+     * 设置最大可以equals的次数
+     *
+     * @param maxEqualsCount
+     */
+    public synchronized void setMaxEqualsCount(int maxEqualsCount)
+    {
+        mEqualsBlocker.setMaxEqualsCount(maxEqualsCount);
+    }
+
+    /**
+     * 触发对象拦截
+     *
+     * @param object
+     * @return true-拦截掉
+     */
+    public synchronized boolean blockObject(Object object)
+    {
+        if (mEqualsBlocker.blockObject(object))
+        {
+            if (isInBlockEqualsDuration())
+            {
+                return true;
+            }
+        }
+
+        saveLastBlockTime();
+        return false;
+    }
+}
