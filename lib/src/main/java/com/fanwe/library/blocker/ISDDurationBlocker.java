9d2f118a6d581cadabe15040aae17f900ef2b345
zjun <565061763@qq.com>
2017-08-16 13:08:57 +0800

    重构

---
+++ lib/src/main/java/com/fanwe/library/blocker/ISDDurationBlocker.java
@@ -0,0 +1,63 @@
+package com.fanwe.library.blocker;
+
+/**
+ * Created by Administrator on 2017/8/16.
+ */
+public interface ISDDurationBlocker
+{
+    /**
+     * 设置拦截间隔
+     *
+     * @param blockDuration 拦截间隔（毫秒）
+     */
+    void setBlockDuration(long blockDuration);
+
+    /**
+     * 获得拦截间隔（毫秒）
+     *
+     * @return
+     */
+    long getBlockDuration();
+
+    /**
+     * 保存最后一次通过拦截的合法时间点
+     */
+    void saveLastLegalTime();
+
+    /**
+     * 返回最后一次通过拦截的合法时间点
+     *
+     * @return
+     */
+    long getLastLegalTime();
+
+    /**
+     * 设置是否自动保存最后一次触发拦截的时间，默认自动保存
+     *
+     * @param autoSaveLastLegalTime true-自动保存
+     */
+    void setAutoSaveLastLegalTime(boolean autoSaveLastLegalTime);
+
+    /**
+     * 当前是否处于拦截的间隔之内
+     *
+     * @param blockDuration 拦截间隔（毫秒）
+     * @return true-是
+     */
+    boolean isInBlockDuration(long blockDuration);
+
+    /**
+     * 触发拦截
+     *
+     * @return true-拦截掉
+     */
+    boolean block();
+
+    /**
+     * 触发拦截
+     *
+     * @param blockDuration 拦截间隔（毫秒）
+     * @return true-拦截掉
+     */
+    boolean block(long blockDuration);
+}
--- lib/src/main/java/com/fanwe/library/blocker/SDDurationBlocker.java
+++ lib/src/main/java/com/fanwe/library/blocker/SDDurationBlocker.java
@@ -3,7 +3,7 @@ package com.fanwe.library.blocker;
 /**
  * 可以根据时间间隔来拦截事件的类
  */
-public class SDDurationBlocker
+public class SDDurationBlocker implements ISDDurationBlocker
 {
     /**
      * 默认拦截间隔
@@ -34,49 +34,7 @@ public class SDDurationBlocker
         setBlockDuration(blockDuration);
     }
 
-    /**
-     * 获得拦截间隔（毫秒）
-     *
-     * @return
-     */
-    public long getBlockDuration()
-    {
-        return mBlockDuration;
-    }
-
-    /**
-     * 返回最后一次通过拦截的合法时间点
-     *
-     * @return
-     */
-    public long getLastLegalTime()
-    {
-        return mLastLegalTime;
-    }
-
-    /**
-     * 保存最后一次通过拦截的合法时间点
-     */
-    public synchronized void saveLastLegalTime()
-    {
-        mLastLegalTime = System.currentTimeMillis();
-    }
-
-    /**
-     * 设置是否自动保存最后一次触发拦截的时间，默认自动保存
-     *
-     * @param autoSaveLastLegalTime true-自动保存
-     */
-    public synchronized void setAutoSaveLastLegalTime(boolean autoSaveLastLegalTime)
-    {
-        mAutoSaveLastLegalTime = autoSaveLastLegalTime;
-    }
-
-    /**
-     * 设置拦截间隔
-     *
-     * @param blockDuration 拦截间隔（毫秒）
-     */
+    @Override
     public synchronized void setBlockDuration(long blockDuration)
     {
         if (blockDuration < 0)
@@ -86,34 +44,44 @@ public class SDDurationBlocker
         mBlockDuration = blockDuration;
     }
 
-    /**
-     * 当前是否处于拦截的间隔之内
-     *
-     * @param blockDuration 拦截间隔（毫秒）
-     * @return true-是
-     */
+    @Override
+    public long getBlockDuration()
+    {
+        return mBlockDuration;
+    }
+
+    @Override
+    public synchronized void saveLastLegalTime()
+    {
+        mLastLegalTime = System.currentTimeMillis();
+    }
+
+    @Override
+    public long getLastLegalTime()
+    {
+        return mLastLegalTime;
+    }
+
+    @Override
+    public synchronized void setAutoSaveLastLegalTime(boolean autoSaveLastLegalTime)
+    {
+        mAutoSaveLastLegalTime = autoSaveLastLegalTime;
+    }
+
+    @Override
     public synchronized boolean isInBlockDuration(long blockDuration)
     {
         long duration = System.currentTimeMillis() - mLastLegalTime;
         return duration < blockDuration;
     }
 
-    /**
-     * 触发拦截
-     *
-     * @return true-拦截掉
-     */
+    @Override
     public boolean block()
     {
         return block(mBlockDuration);
     }
 
-    /**
-     * 触发拦截
-     *
-     * @param blockDuration 拦截间隔（毫秒）
-     * @return true-拦截掉
-     */
+    @Override
     public synchronized boolean block(long blockDuration)
     {
         if (isInBlockDuration(blockDuration))
