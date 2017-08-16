a7cb8513c6c7db7cc9ca7f17984142751d6c6eb6
zjun <565061763@qq.com>
2017-08-16 09:57:44 +0800

    重构

--- lib/src/main/java/com/fanwe/library/blocker/SDEqualsBlocker.java
+++ lib/src/main/java/com/fanwe/library/blocker/SDEqualsBlocker.java
@@ -6,9 +6,9 @@ package com.fanwe.library.blocker;
 public class SDEqualsBlocker
 {
     /**
-     * 最后一次保存的对象
+     * 最后一次通过拦截的合法对象
      */
-    private Object mLastObject = new Object();
+    private Object mLastLegalObject = new Object();
     /**
      * 最大可以equals的次数
      */
@@ -17,6 +17,10 @@ public class SDEqualsBlocker
      * 当前equals的次数
      */
     private int mEqualsCount;
+    /**
+     * 是否自动保存最后一次通过拦截的合法对象，默认自动保存
+     */
+    private boolean mAutoSaveLastLegalObject = true;
 
     /**
      * 设置最大可以equals的次数
@@ -29,6 +33,16 @@ public class SDEqualsBlocker
     }
 
     /**
+     * 设置是否自动保存最后一次通过拦截的合法对象，默认自动保存
+     *
+     * @param autoSaveLastLegalObject true-自动保存
+     */
+    public synchronized void setAutoSaveLastLegalObject(boolean autoSaveLastLegalObject)
+    {
+        mAutoSaveLastLegalObject = autoSaveLastLegalObject;
+    }
+
+    /**
      * 触发对象拦截
      *
      * @param object
@@ -36,7 +50,7 @@ public class SDEqualsBlocker
      */
     public synchronized boolean blockObject(Object object)
     {
-        if (mLastObject.equals(object))
+        if (mLastLegalObject.equals(object))
         {
             mEqualsCount++;
             if (mEqualsCount > mMaxEqualsCount)
@@ -44,12 +58,13 @@ public class SDEqualsBlocker
                 mEqualsCount--;
                 return true;
             }
-        } else
-        {
-            resetEqualsCount();
         }
 
-        saveLastObject(object);
+        resetEqualsCount();
+        if (mAutoSaveLastLegalObject)
+        {
+            saveLastLegalObject(object);
+        }
         return false;
     }
 
@@ -61,8 +76,13 @@ public class SDEqualsBlocker
         mEqualsCount = 0;
     }
 
-    private void saveLastObject(Object lastObject)
+    /**
+     * 保存最后一次通过拦截的合法对象
+     *
+     * @param lastLegalObject
+     */
+    public synchronized void saveLastLegalObject(Object lastLegalObject)
     {
-        this.mLastObject = lastObject;
+        mLastLegalObject = lastLegalObject;
     }
 }
